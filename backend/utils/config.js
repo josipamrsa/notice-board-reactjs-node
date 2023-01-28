33b22219bb6ebdc77997fbcb6e4d99d35b0fd20b
Josipa Mrsa <josipa.mrsa@atos.net>
2023-01-28 13:18:29 +0100

    update

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -8,11 +8,12 @@ const PORT = process.env.PORT;
 
 //----BAZA PODATAKA----//
 const password = process.env.ATLAS_PASS;
+const username = process.env.ATLAS_USER;
 
 const dbname = process.env.NODE_ENV === 'test'
 ? 'ob-ploca-test'
 : 'ob-ploca-api';
 
-const DB_URI = `mongodb+srv://jmrsa-oarwa:${password}@obavijesnaplocacluster.p31wx.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const DB_URI = `mongodb+srv://${username}:${password}@obavijesnaplocacluster.p31wx.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 module.exports = {PORT, DB_URI};
 
